395c9ab972e8fd51935aa85fdd72bbd55b0488d4
Anna <108423045+staikova-engineer@users.noreply.github.com>
2022-08-23 10:59:34 +0300

    little changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
       <div className="container">
         <Weather />
       </div>
-      <footer>This react project was coded by <a href="https://www.linkedin.com/in/anna-staikova-engineer/" target="_blank">Anna Staikova</a> and is <a href="https://github.com/staikova-engineer/react-weather-app" target="_blank">open-sourced</a> on GitHub</footer>
+      <footer>This react project was coded by <a href="https://www.linkedin.com/in/anna-staikova-engineer/" target="_blank">Anna Staikova</a> and is <a href="https://github.com/staikova-engineer/react-weather" target="_blank">open-sourced</a> on GitHub</footer>
     </div>
   );
 }
