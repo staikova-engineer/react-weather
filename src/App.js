0756561c667d7a6e11e518b6c59aa29349597c7f
Anna <108423045+staikova-engineer@users.noreply.github.com>
2022-09-05 17:17:13 +0300

    added link on Netlify on footer

--- src/App.js
+++ src/App.js
@@ -12,7 +12,32 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Tallinn" />
       </div>
-      <footer>This react project was coded by <a href="https://www.linkedin.com/in/anna-staikova-engineer/" target="_blank" rel="noreferrer">Anna Staikova</a> and is <a href="https://github.com/staikova-engineer/react-weather" target="_blank" rel="noreferrer">open-sourced</a> on GitHub</footer>
+      <footer>
+        This react project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/anna-staikova-engineer/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Anna Staikova
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/staikova-engineer/react-weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub and hosted on{" "}
+        <a
+          href="https://creative-youtiao-d48ab9.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
